00bddcf2187fd819daaa5cb7854de87c06e39b2b
Naufal Fadriansyah <95693500f@gmail.com>
2020-03-29 16:36:16 +0700

    add style jumbotron

--- src/component/jumbotron/jumb.jsx
+++ src/component/jumbotron/jumb.jsx
@@ -4,7 +4,7 @@ import './jumb.css'
 export default function Jumbotron(props) {
     return(
         <div className="jumbotron jumbotron-fluid">
-  <div className="container">
+  <div className="container text-center">
     <h1 className="display-4 text-white">Turn your <span>Design</span> into <br></br>profesional <span>Code</span> with us</h1>
     <a className="nav-item btn btn-light button" href="#">Our Work</a>
   </div>
