2757b84a87f81f99134392742ef1646b4597345d
Naufal Fadriansyah <95693500f@gmail.com>
2020-03-29 09:44:04 +0700

    add panel

---
+++ src/component/panel/panel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './panel.css'
+export default function panel(props) {
+    return(
+        <div>
+            <div className="row justify-content-center">
+                <div className="col-lg-10 info-panel">
+                    <div className="row">
+                        <div className="col">
+                            <img src="../../aset/layers.png" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
