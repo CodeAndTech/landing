ecb0557fe90929e9ccd89cb03f035fa026f9040b
Naufal Fadriansyah <95693500f@gmail.com>
2020-03-29 10:55:45 +0700

    space done!

--- src/component/Space/space.jsx
+++ src/component/Space/space.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Prof from '../../aset/profesional.jpg'
 import './space.css'
 export default function Space(props) {
-    return(
+    return (
         <div className="container">
             <div className="row">
-                <div className="col-lg-6">
-                    <img src={Prof}alt="" className="img-fluid"/>
+                <div className="col-lg-5 space">
+                    <img src={Prof} alt="" className="img-fluid" />
+                </div>
+                <div className="col-lg-5 space text-left">
+                    <h3>we have a <span>professional</span> crew</h3>
+                    <p>Turn your design to high quality code with us</p>
+                    <a href="#" className="btn btn-dark button">Send Your Project</a>
                 </div>
             </div>
         </div>
