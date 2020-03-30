b1398aebe71f47c482793845e5540975df7dfea9
Naufal Fadriansyah <95693500f@gmail.com>
2020-03-30 18:26:51 +0700

    add data

---
+++ src/component/quote/data.js
@@ -0,0 +1,25 @@
+const data = {
+    app: [
+        {
+            "id": 1,
+            "index": 1,
+            "quote": `“Before we even did a kickoff call, I knew we'd make the right choice to work with the codeandtech team. Their body of work and influence on the industry speak volumes.”`,
+            "nama": 'Alice max',
+            "profesi": 'Designer'
+        },
+        {
+            "id": 2,
+            "index": 2,
+            "quote": `“ini quoute 2”`,
+            "nama": 'Alice max 2',
+            "profesi": 'Designer 2'
+        }, 
+        {
+            "id": 3,
+            "index": 3,
+            "quote": `“ini quoute 3”`,
+            "nama": 'Alice max 3',
+            "profesi": 'Designer 3'
+        }
+    ]
+}
